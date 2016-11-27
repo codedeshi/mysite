6e6a108fcaf85974d976858a3d42a580347ec04d
codedeshi <singh.hardit@gmail.com>
2016-11-27 00:28:07 -0600

    removed unnessasary libraries

--- bower_components/angular-gist/examples/simple-demo/app.js
+++
@@ -1,9 +0,0 @@
-
-angular.module('gistDemoApp', ['gist'])
-    .controller('mainController', MainController);
-
-function MainController() {
-    var vm = this;
-
-    vm.gistId = 'e65d546c16acb1a10c3f5e1f673c5223';
-}
